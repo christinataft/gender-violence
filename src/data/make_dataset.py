773ca2255a5b9ddc7f332e9d90c85ef904a3628d
yabramuvdi <yabran.muvdi@gmail.com>
2020-08-23 20:46:02 +0200

    update

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,5 +5,5 @@ import preprocess_raw_data
 
 if __name__ == '__main__':
     extract_data.get_spreadsheet()
-    #extract_text.get_text()
+    extract_text.get_text()
     preprocess_raw_data.preprocess()
